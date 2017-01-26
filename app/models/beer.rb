277b49eb222fa96cba4b4beb1efdd64af369a957
Henri Piironen <henri.piironen@gmail.com>
2017-01-26 22:00:24 +0200

    toka viikko loppuun

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,13 +1,8 @@
 class Beer < ActiveRecord::Base
-	belongs_to :brewery
-	has_many :ratings
+	include RatingAverage
 
-	def average_rating
-		list = Array.new
-		ratings.each { |r| list.push(r.score)}
-		sum = list.inject { |sum, n| sum + n }
-		sum / ratings.count
-	end
+	belongs_to :brewery
+	has_many :ratings, dependent: :destroy
 
 	def to_s
 		
