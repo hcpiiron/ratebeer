41c5876d0d5027b5c7ff3dd4e35e268b49635628
Henri Piironen <henri.piironen@gmail.com>
2017-01-26 18:25:02 +0200

    tokan viikon 8 ekaa

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,17 @@
 class Beer < ActiveRecord::Base
 	belongs_to :brewery
 	has_many :ratings
+
+	def average_rating
+		list = Array.new
+		ratings.each { |r| list.push(r.score)}
+		sum = list.inject { |sum, n| sum + n }
+		sum / ratings.count
+	end
+
+	def to_s
+		
+		(self.name + " brewed by " + brewery.name)
+	end
+
 end
